aaf6d996dbed33e3bc0339d5e2f211fb930b5539
Shota Yoshida <yoshida.shota@growfit.co.jp>
2025-11-20 14:53:52 +0900

    deploy test

--- lambda-python/app.py
+++ lambda-python/app.py
@@ -4,3 +4,10 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": "Hello from Python Lambda!"
     }
+
+# ローカル実行用
+if __name__ == "__main__":
+    fake_event = {"foo": "bar"}
+    fake_context = None  # 必要ならダミークラス作る
+    res = lambda_handler(fake_event, fake_context)
+    print(res)
